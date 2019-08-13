2ef7b03b4988bbcd3936b6be93af2c9acfedb570
Eli Miles <elimiles1987@gmail.com>
2019-08-13 22:03:27 +0300

    insert one and retrive all

--- server/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI:'mongodb+srv://EliM:WPdvYHV@yEnf29M@comments-44zbq.mongodb.net/test?retryWrites=true&w=majority'
-};
--- server/index.js
+++ server/index.js
@@ -1,14 +1,52 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
-const keys = require('./config/keys');
+const express = require('express')
+const app = express()
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const bodyParser = require('body-parser')
 
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'comments-manager'
 
-mongoose.connect(keys.mongoURI);
+app.use( bodyParser.json() )
+app.use(bodyParser.urlencoded( {extended: true} ))
 
-app.get('/', (req,res) => {
-    res.send({ hi: 'Eli' });
-});
+app.post('/insert', (req,res) => {
+
+    MongoClient.connect(connectionURL, { useNewUrlParser:true }, (error, client) => {
+        if (error){
+            return console.log('Unable to connect to database!')
+        }
+
+        const db = client.db(databaseName)
+
+        db.collection('comments').insertOne({
+            email: req.body.email,
+            comment: req.body.comment,
+            rating:req.body.rating
+        })
+    })
+
+    res.send({ message: 'comment was entered successfully from the user' })
+})
+
+app.get('/comments', (req,res) => {
+
+    MongoClient.connect(connectionURL, { useNewUrlParser:true }, (error, client) => {
+        if (error){
+            return console.log('Unable to connect to database!')
+        }
+
+        const db = client.db(databaseName)
+
+        db.collection('comments').find({}).toArray( (err, result) => {
+            if (err) {
+                console.log('Did not found any comments')
+            }
+
+            res.send(result)
+        })
+    })
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
